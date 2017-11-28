e4c60adee61ca3126899fb4ba131af49d4e2003d
Andrea Caldera <andrea.caldera@gmail.com>
2017-11-28 15:20:41 +0000

    lint

--- src/common/components/Proxy.js
+++ src/common/components/Proxy.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { get } from 'lodash';
 import moment from 'moment';
 
 import proxyModule from '../modules/proxy';
