f78dc07ad4b31a44784195dcffcecfb60c98df4b
Mudi <mudi@renegare.com>
2017-12-05 20:37:31 +0000

    lint

--- test/util/async.js
+++ test/util/async.js
@@ -1,11 +1,11 @@
 export const find = async (collection, iteratee) => {
   let found = null;
   await Promise.all(
-    collection.map(item => {
-      return Promise.resolve(iteratee(item)).then(match => {
+    collection.map(item =>
+      Promise.resolve(iteratee(item)).then(match => {
         if (!found && match) found = item;
-      });
-    }),
+      }),
+    ),
   );
   return found;
 };
