2f44bfa7ccb7fb5fa81223a1a982aa113d3ce233
Andrea Caldera <andrea.caldera@gmail.com>
2017-12-07 10:47:49 +0000

    proxy config displayed in about page

--- src/common/components/About.js
+++ src/common/components/About.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-const About = () => (
+import proxyModule from '../modules/proxy';
+
+const About = ({ proxyConfig }) => (
   <div>
     <h1>About</h1>
     <p>Welcome to Bluffer, a proxy mock thingy</p>
+    <ul className="container list-group">
+      {proxyConfig.map((config) => (
+        <li key={config.port} className="row list-group-item">
+          <div className="col">{config.name}</div>
+          <div className="col">Listening on: {config.port}</div>
+          <div className="col">Targetting: {config.target}</div>
+        </li>
+      ))}
+    </ul>
   </div>
 );
 
-export default connect(null, null)(About);
+About.propTypes = {
+  proxyConfig: PropTypes.arrayOf(PropTypes.shape().isRequired).isRequired,
+};
+
+const mapStateToProps = state => ({
+  proxyConfig: proxyModule.getConfig(state),
+});
+
+export default connect(mapStateToProps, null)(About);
