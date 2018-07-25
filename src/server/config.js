f1794cea727bf310a701c35c758c4d0b18d90030
Andrea Caldera <andrea.calderagmail.com>
2018-07-25 16:22:21 +0100

    default config

--- src/server/config.js
+++ src/server/config.js
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import { merge } from 'lodash';
 import logger from './logger';
 
 const blufferConfig = process.env.BLUFFER_CONFIG;
@@ -6,6 +7,11 @@ if (!blufferConfig) {
   logger.warn('You forgot to set BLUFFER_CONFIG, defaulting to local');
 }
 
-const config = JSON.parse(fs.readFileSync(`./config/${blufferConfig || 'local'}.json`, 'utf8'));
+const config = merge(
+  JSON.parse(fs.readFileSync('./config/default.json', 'utf8')),
+  JSON.parse(fs.readFileSync(`./config/${blufferConfig || 'local'}.json`, 'utf8'))
+);
+
+config.mongodb.password = process.env.MONGODB_PASSWORD;
 
 export default config;
--- src/server/stores/index.js
+++ src/server/stores/index.js
@@ -4,13 +4,23 @@ import mongoose from 'mongoose';
 import proxiedResponsesStoreFactory from './proxied-responses-store';
 import mockedResponsesStoreFactory from './mocked-responses-store';
 
+const userAndPassword = ({ user, password }) => {
+  if (!user && !password) {
+    return '';
+  }
+  if (user && !password) {
+    return `${user}@`;
+  }
+
+  return `${user}:${password}@`;
+};
+
 export default ({ mongodb }) => {
   mongoose.Promise = bluebird;
   const replicaSet = mongodb.replicaSet ? `&replicaSet=${mongodb.replicaSet}` : '';
   const ssl = `ssl=${mongodb.ssl}`;
   const authSource = mongodb.authSource ? `&authSource=${mongodb.authSource}` : '';
-  const userAndPassword = mongodb.user ? `${mongodb.user}:${process.env.MONGODB_PASSWORD}@` : '';
-  const url = `mongodb://${userAndPassword}${mongodb.hosts}/${mongodb.database}?${ssl}${replicaSet}${authSource}`;
+  const url = `mongodb://${userAndPassword(mongodb)}${mongodb.hosts}/${mongodb.database}?${ssl}${replicaSet}${authSource}`;
 
   const closeStoreConnection = () => mongoose.connection.close();
 
